b174e2daa346bc66da2a82c1de35e477e78385ec
Volodia Nudyk <volodianudyk@gmail.com>
2022-09-23 00:00:31 +0300

    Modefied HW3

--- HW3/V_Nudyk/changing.py
+++ HW3/V_Nudyk/changing.py
@@ -6,6 +6,7 @@ print("b =", number_b)
 
 print("Now lets change the two numbers between themselves: ")
 
+#The task is to change the nums without the third var
 number_a, number_b = number_b, number_a
 
 print("a =", number_a, "\nand \nb =", number_b)
