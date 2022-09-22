7ce04ce9b8b9be9cfd8e42e4bf83a9a72d0b4c71
Volodia Nudyk <volodianudyk@gmail.com>
2022-09-22 23:37:53 +0300

    Added HW

---
+++ HW3/V_Nudyk/changing.py
@@ -0,0 +1,11 @@
+number_a = int(input("Give me a number that I will store as a: "))
+number_b = int(input("Give me another number that I will recognize as b: "))
+
+print("a =", number_a)
+print("b =", number_b)
+
+print("Now lets change the two numbers between themselves: ")
+
+number_a, number_b = number_b, number_a
+
+print("a =", number_a, "\nand \nb =", number_b)
