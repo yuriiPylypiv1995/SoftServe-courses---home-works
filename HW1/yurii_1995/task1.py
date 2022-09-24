93273480a89f3d640d491211ca386970dc7fd9fc
Yurii Pylypiv <yra.pilipsv@gmail.com>
2022-09-24 13:06:12 +0300

    Home work 1 tasks added

---
+++ HW1/yurii_1995/task1.py
@@ -0,0 +1,9 @@
+# HW1, task 1
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+place = input("Where do you live? ")
+
+print(f"Hello, {name}")
+print(f"Your age is {age}")
+print(f"You live in {place}")
