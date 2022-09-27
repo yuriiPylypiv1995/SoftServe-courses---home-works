6ae3f7bb559dbc3a13c6f7f68b826a67b8ea86a3
Yurii Sabadash <yurasabadash54@gmail.com>
2022-09-27 16:00:32 +0300

    Added HW1

--- HW1/YSaba/task1.py
+++ HW1/YSaba/task1.py
@@ -1,1 +1,8 @@
-print("Hello World")
+#HW1 , task 1
+
+name = (input(" What is your name? "))
+age = int(input(" How old are you? "))
+residence = (input(" Where do you live? "))
+print(f"Hello , {name} ")
+print(f"Your age is {age} ")
+print(f"You live in {residence} ")
