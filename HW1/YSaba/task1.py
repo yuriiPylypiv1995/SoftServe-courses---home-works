37ec7e4e4696f794014416d729049c65f8237e98
Yurii Sabadash <yurasabadash54@gmail.com>
2022-09-27 16:30:51 +0300

    idk

--- HW1/YSaba/task1.py
+++
@@ -1,8 +0,0 @@
-#HW1 , task 1
-
-name = (input(" What is your name? "))
-age = int(input(" How old are you? "))
-residence = (input(" Where do you live? "))
-print(f"Hello , {name} ")
-print(f"Your age is {age} ")
-print(f"You live in {residence} ")
--- HW1/YSaba/task2.py
+++
@@ -1,9 +0,0 @@
-#HW 1 , task 2
-
-a = int(input("Enter a number_1 "))
-b = int(input("Enter a number_2 "))
-print(f"The sum of {a} and {b} :",a + b )
-print(f"The subrtaction of {a} and {b} :",a - b)
-print(f"The multiplication of {a} and {b} :",a - b)
-print(f"The division of {a} and {b} : ",a / b)
-print(f"The exponentiation of {a} and {b}: ",a**b)
