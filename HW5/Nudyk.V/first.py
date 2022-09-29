6ac60022e05f40d9ac56ab51c3e59f1f6a8ae9c9
Volodia Nudyk <volodianudyk@gmail.com>
2022-09-29 22:01:51 +0300

    Added HW -> first.py

---
+++ HW5/Nudyk.V/first.py
@@ -0,0 +1,14 @@
+# Write a script that checks the login that the user enters.
+# If the login is "First", then greet the users. If the login is
+# different, send an error message.
+# (need to use loop while)
+
+
+while True:
+    user_login = input('Enter your login: ')      
+    if user_login == 'First':
+        print(f'Hello, {user_login} user !')
+        break
+    else:
+        print('Error: enter correct login !')
+        # continue                                # Using to continue the    
