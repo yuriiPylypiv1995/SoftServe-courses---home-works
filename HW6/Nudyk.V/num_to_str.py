8b7402495999a24ee3f1b210b2a5fb1493c26156
Volodia Nudyk <volodianudyk@gmail.com>
2022-09-30 19:41:15 +0300

    Added comments

--- HW6/Nudyk.V/num_to_str.py
+++ HW6/Nudyk.V/num_to_str.py
@@ -1,6 +1,7 @@
 # We need a function that can transform a number (integer) into a string.
 
 
+# Format the int into string
 def number_to_string(num):
     return (f'{num}')
 
@@ -8,11 +9,9 @@ def number_to_string(num):
 print(number_to_string(10+10))
 
 
+# Convert int into str
 def number_to_string_v2(num):
     return str(num)
 
 
 print(number_to_string_v2(10+10))
-
-
-
