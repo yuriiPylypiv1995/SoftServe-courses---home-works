e75133ccffbb4cd9dafee359a966ce2f61a6694b
Yurii Pylypiv <yra.pilipsv@gmail.com>
2022-10-01 22:10:58 +0300

    home work 5, task 1 added

---
+++ HW5/yurii_1995/task1.py
@@ -0,0 +1,30 @@
+# home work 5, task 1
+
+numbers = range(1, 11)
+
+even_numbers = []
+odd_numbers = []
+not_divisible_numbers = []
+
+for number in numbers:
+    if number % 2 == 0:
+        even_numbers.append(number)
+    else:
+        pass
+
+for number in numbers:
+    if number % 3 == 0 and number % 2 != 0:
+        odd_numbers.append(number)
+    else:
+        pass
+
+for number in numbers:
+    if number % 2 != 0 and number % 3 != 0:
+        not_divisible_numbers.append(number)
+    else:
+        pass
+
+print(even_numbers)
+print(odd_numbers)
+print(not_divisible_numbers)
+
