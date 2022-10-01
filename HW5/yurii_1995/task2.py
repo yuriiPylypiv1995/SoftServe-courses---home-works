6abf884d3685ed4760dcabeb27e84a85472da571
Yurii Pylypiv <yra.pilipsv@gmail.com>
2022-10-01 21:43:36 +0300

    home work 5, task 2 added. Also from gitignore .py deleted

---
+++ HW5/yurii_1995/task2.py
@@ -0,0 +1,11 @@
+# home work 5, task 2
+
+user_login = input("Enter your login: ")
+
+while True:
+    if user_login == "First":
+        print("Hello, First!")
+        break
+    else:
+        print("The entered login is invalid. Try again")
+        user_login = input("Enter your login: ")
