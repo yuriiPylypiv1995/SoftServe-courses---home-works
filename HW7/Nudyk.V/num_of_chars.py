b9e786278eae418688455bfb879bb47b5f5a7c51
Volodia Nudyk <volodianudyk@gmail.com>
2022-10-03 17:53:27 +0300

    Added HW7 - num_of_chars.py

--- HW7/Nudyk.V/num_of_chars.py
+++ HW7/Nudyk.V/num_of_chars.py
@@ -5,7 +5,14 @@
 
 
 def nums_of_char(text):
-    
+    # Created an empty dictionary
+    text_dict = {} 
+    # Changed the type of the text into list
+    text = list(text)
+    # Loop for every char to get it counted
+    for char in text:
+        text_dict[char] = text.count(char)
+    return text_dict
 
-print(nums_of_char('Hello'))
+print(nums_of_char("hello"))
 
