5bb6dd20dbe636d860a363940295ed644e04f94c
Vjacheslav Golovach <syava_trader@ukr.net>
2022-10-09 12:26:39 +0300

    Calculator's implementation

---
+++ HW8/calculator.py
@@ -0,0 +1,33 @@
+def mult(one, two):
+    return one * two
+
+def dev(one, two):
+    return one / two
+
+def sum(one, two):
+    return one + two
+
+def sub(one, two):
+    return one - two
+
+# def mult(one, two):
+#     return one*two
+
+# def mult(one, two):
+#     return one*two
+
+collection = []
+
+
+
+def collect(quntity):
+    i = 1
+    while i <= quntity:
+        collection.append(int(input("Enter your number:")))
+        i += 1
+    return collection
+
+def run_through_the_loop():
+    pass
+
+# print(collect(3))
