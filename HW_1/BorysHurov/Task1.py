73a62c787a90c5f0a78978f335a54b317d8e7027
Borys.hurov <borys.hurov@gmail.com>
2022-10-11 15:50:49 +0300

    add hw1 folder

---
+++ HW_1/BorysHurov/Task1.py
@@ -0,0 +1,7 @@
+from traceback import print_exc
+userName =input("What is your name? ")
+how_old_are_you =input("How old are you?")
+Where_do_you_live =input("Where do you live?")
+print("Hello",userName)
+print("Your age is" ,how_old_are_you )
+print("You live in" ,Where_do_you_live)
---
+++ HW_1/BorysHurov/Task2.py
@@ -0,0 +1,8 @@
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+
+print("додавання",a+b)
+print("віднімання",a-b)
+print("множення",a*b)
+print("ділення",(a/b))
+print("ступінь",a**b)
