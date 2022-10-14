a0953156c10ce63ce4f4518f8e1f2bdd1e5a013e
Borys.hurov <borys.hurov@gmail.com>
2022-10-14 15:55:55 +0300

    replace task1 from HW2 to HW3

---
+++ HW_3/Borys_Hurov/Task1.py
@@ -0,0 +1,23 @@
+from operator import truediv
+from re import I
+import this
+from xml.etree.ElementTree import PI
+from xmlrpc.client import boolean
+import this
+import codecs
+
+zen_of_python = codecs.encode(this.s, 'rot13')
+print(zen_of_python)
+print('=================================================')
+zen_of_python=zen_of_python.upper()
+print(zen_of_python)
+print('=================================================')
+countIs = zen_of_python.count(" IS ")
+countBetter = zen_of_python.count("BETTER")
+countNever = zen_of_python.count("NEVER")
+print("count of word 'is'=",countIs)
+print("count of word 'Better'=",countBetter)
+print("count of word 'Never'=",countNever)
+print('=================================================')
+zen_of_python=zen_of_python.replace("I","&")
+print(zen_of_python)
