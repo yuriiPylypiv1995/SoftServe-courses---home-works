7f0aa67c468ac663272215201fb81443b65871ed
Vjacheslav Golovach <syava_trader@ukr.net>
2022-10-15 19:51:24 +0300

    color chost added

---
+++ HW11/Holovach/color chost.py
@@ -0,0 +1,17 @@
+import random
+
+
+class Ghost(object):
+
+    def __init__(self) -> None:
+        self.color = random.choice(["white", "yellow", "purple", "red"])
+
+    # def color(self):
+    #     return random.choice(["white", "yellow", "purple", "red"])
+
+print(Ghost().color)
+
+# def color():
+#         return random.choice(["white", "yellow", "purple", "red"])
+
+# print(color())
